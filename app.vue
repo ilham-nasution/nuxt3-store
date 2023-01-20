beee4be8d118120f68cab4ac0f747d69d50c1e5e
ilham-nasution <ilhamnasution@Admins-MacBook-Air-3.local>
2023-01-20 16:00:58 +0700

    add signup page

--- app.vue
+++ app.vue
@@ -1,24 +1,3 @@
 <template>
-  <div class="h-screen grid place-items-center">
-    <div class="card w-96 bg-base-100 shadow-xl">
-      <div class="card-body">
-        <h2 class="card-title justify-center">Login</h2>
-        <div class="form-control w-full max-w-xs">
-          <label class="label">
-            <span class="label-text">Email</span>
-          </label>
-          <input type="text" placeholder="Type here" class="input input-bordered w-full max-w-xs" />
-        </div>
-        <div class="form-control w-full max-w-xs">
-          <label class="label">
-            <span class="label-text">Password</span>
-          </label>
-          <input type="text" placeholder="Type here" class="input input-bordered w-full max-w-xs" />
-        </div>
-        <div class="card-actions justify-center">
-          <button class="btn btn-primary">Sign in</button>
-        </div>
-      </div>
-    </div>
-  </div>
+  <NuxtPage />
 </template>
---
+++ pages/signup.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="h-screen grid place-items-center">
+    <div class="card w-96 bg-base-100 shadow-xl">
+      <div class="card-body">
+        <h2 class="card-title justify-center">Signup</h2>
+        <div class="form-control w-full max-w-xs">
+          <label class="label">
+            <span class="label-text">Email</span>
+          </label>
+          <input type="text" placeholder="Type here" class="input input-bordered w-full max-w-xs" />
+        </div>
+        <div class="form-control w-full max-w-xs">
+          <label class="label">
+            <span class="label-text">Password</span>
+          </label>
+          <input type="text" placeholder="Type here" class="input input-bordered w-full max-w-xs" />
+        </div>
+        <div class="card-actions justify-center">
+          <button class="btn btn-primary">Signup</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
