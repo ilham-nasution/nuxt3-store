096c2226429bb39ae41936b10e63c95f42210462
ilham-nasution <ilhamnasution@Admins-MacBook-Air-3.local>
2023-01-20 15:43:10 +0700

    create login ui

--- app.vue
+++ app.vue
@@ -1,5 +1,24 @@
 <template>
-  <h1 class="text-3xl text-green-500 font-bold underline">
-    NUXT 3 + TAILWIND CSS
-  </h1>
+  <div class="h-screen grid place-items-center">
+    <div class="card w-96 bg-base-100 shadow-xl">
+      <div class="card-body">
+        <h2 class="card-title justify-center">Login</h2>
+        <div class="form-control w-full max-w-xs">
+          <label class="label">
+            <span class="label-text">Email</span>
+          </label>
+          <input type="text" placeholder="Type here" class="input input-bordered w-full max-w-xs" />
+        </div>
+        <div class="form-control w-full max-w-xs">
+          <label class="label">
+            <span class="label-text">Password</span>
+          </label>
+          <input type="text" placeholder="Type here" class="input input-bordered w-full max-w-xs" />
+        </div>
+        <div class="card-actions justify-center">
+          <button class="btn btn-primary">Sign in</button>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
