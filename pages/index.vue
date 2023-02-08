a6029c2386a1440975fce5be71cd0f3cf5dc6c80
Ilham Rahman <ilhaamrahman@gmail.com>
2023-02-08 15:50:34 +0700

    create navbar component

--- pages/index.vue
+++ pages/index.vue
@@ -1,68 +1,6 @@
 <template>
-  <div class="navbar bg-base-100">
-    <div class="flex-1">
-      <a class="btn btn-ghost normal-case text-xl">Store</a>
-    </div>
-    <div class="flex-none">
-      <div class="dropdown dropdown-end">
-        <label tabindex="0" class="btn btn-ghost">
-          <div class="flex items-center">
-            <div class="w-6 rounded-full">
-              <UserIcon />
-            </div>
-            <span class="normal-case">{{ user.email }}</span>
-          </div>
-        </label>
-        <ul
-          tabindex="0"
-          class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
-        >
-          <li>
-            <a class="justify-between">
-              Profile
-              <span class="badge">New</span>
-            </a>
-          </li>
-          <li><a>Settings</a></li>
-          <li><a @click="handleLogout">Logout</a></li>
-        </ul>
-      </div>
-      <div class="dropdown dropdown-end">
-        <label tabindex="0" class="btn btn-ghost btn-circle">
-          <div class="indicator">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              class="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
-              />
-            </svg>
-            <span class="badge badge-sm indicator-item">8</span>
-          </div>
-        </label>
-        <div
-          tabindex="0"
-          class="mt-3 card card-compact dropdown-content w-52 bg-base-100 shadow"
-        >
-          <div class="card-body">
-            <span class="font-bold text-lg">8 Items</span>
-            <span class="text-info">Subtotal: $999</span>
-            <div class="card-actions">
-              <button class="btn btn-primary btn-block">View cart</button>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-  <div class="container mx-auto">
+  <Navbar />
+  <div class="container mx-auto my-5">
     <div class="grid grid-cols-3 gap-4">
       <div v-for="product in products">
         <ProductCard
@@ -76,7 +14,6 @@
 </template>
 
 <script setup>
-import { UserIcon } from "@heroicons/vue/24/outline";
 import { ref } from "vue";
 
 const client = useSupabaseClient();
@@ -92,11 +29,6 @@ watchEffect(async () => {
     await navigateTo("/login");
   }
 });
-
-async function handleLogout() {
-  const { error } = await client.auth.signOut();
-  console.log(error);
-}
 
 onMounted(async () => {
   const { data } = await client.from("products").select("*");
