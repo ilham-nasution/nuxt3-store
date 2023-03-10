ccccd6ddc62bed955d094f1c1efa03b2032f0b02
Ilham Rahman <ilhaamrahman@gmail.com>
2023-03-10 10:02:55 +0700

    add pinia store

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,7 +7,7 @@ export default defineNuxtConfig({
       autoprefixer: {},
     },
   },
-  modules: ["@nuxtjs/supabase", "@nuxt/image-edge"],
+  modules: ["@nuxtjs/supabase", "@nuxt/image-edge", "@pinia/nuxt"],
   image: {
     domains: ["https://hipydpadmuujyavkgdtr.supabase.co"],
   },
